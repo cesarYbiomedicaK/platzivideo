dcb6f2ab7c4ece61410e937629b59609fdcb1de1
Cesar Antonio Castro Pareja <cesarantonio.castro@unmsm.edu.pe>
2021-04-29 09:36:16 -0500

    ESLint

---
+++ src/components/hiworld.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import '../assets/styles/app.scss';
+const Hiworld = () => {
+  return(<h1>Hiworld</h1>)
+
+}
+export default Hiworld;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import Hiworld from './components/hiworld.jsx'
+
+
+
+ReactDOM.render(<Hiworld />, document.getElementById('app'));
